22c4d4d0f441cdc07aff68b5f3ead75be2fd828d
fadiyezminor <mehmet-boz@hotmail.com.tr>
2023-10-09 11:49:35 +0300

    new assets added

---
+++ Assets/PipeMoveScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveScript : MonoBehaviour
+{
+    public float moveSpeed=5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+    }
+}
